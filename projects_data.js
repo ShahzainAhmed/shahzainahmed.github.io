d87eb6d1cc630f6890f1ea3d97e0e3c7608e35fd
Shahzain Ahmed <shahzainahmed57@gmail.com>
2025-10-28 13:32:49 +0500

    Update project descriptions and add new projects in projects_data.js; adjust image styling in index.html

--- projects_data.js
+++ projects_data.js
@@ -51,9 +51,9 @@ const projects = [
         title: "Travel Booking",
         image: "assets/projects/project7.png",
         description:
-            "A dynamic and visually appealing mobile application built with Flutter to showcase a wide range of perfumes and their details. Features an intuitive interface with real-time data integration through models, ensuring smooth navigation and an engaging user experience.",
-        github: "https://github.com/ShahzainAhmed/PerfumeMakerApp",
-        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7063481759492976640/?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A7063481759492976640%29"
+            "An intuitive Flutter-based travel booking app built for effortless trip planning. It features smooth animations, engaging visuals, and data-driven destination details with ratings and descriptions. Users can also favorite destinations to create a personalized travel list.",
+        github: "https://github.com/ShahzainAhmed/TravelBookingApp",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7172603081224798209/"
     },
     {
         title: "Hotel Booking",
@@ -67,8 +67,129 @@ const projects = [
         title: "Kusmi Tea",
         image: "assets/projects/project9.png",
         description:
-            "A Flutter-based blog application featuring a clean, modern UI for an engaging reading experience. It highlights top writers with professional visuals, showcases upcoming releases, and includes a dynamic recent blogs feed that keeps users updated with the latest content in real time.",
-        github: "https://github.com/ShahzainAhmed/BlogApp",
-        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7156735134799405056/?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A7156735134799405056%29"
+            "A beautifully designed Flutter app inspired by Kusmi Tea, offering an elegant and immersive browsing experience. Users can explore a curated selection of teas with stunning visuals, prices, and detailed descriptions, enhanced by smooth animations and efficient data models for accurate and seamless performance.",
+        github: "https://github.com/ShahzainAhmed/KusmiTeaApp",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7180887470878695424/"
+    },
+    // Start here
+    {
+        title: "Travel Adventure",
+        image: "assets/projects/project10.png",
+        description:
+            "A sleek Flutter-based travel app showcasing luxury hotels in Dubai, UAE. Designed for effortless exploration, it features smooth animations, detailed hotel information, stunning visuals, and user ratings to deliver an elegant and seamless booking experience.",
+        github: "https://github.com/ShahzainAhmed/Travel_Adventure",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7249775138819194880/"
+    },
+    {
+        title: "Car Rental BLoC",
+        image: "assets/projects/project11.jpg",
+        description:
+            "A modern Flutter-based car rental app built with the BLoC architecture for a seamless user experience. Car Rental Modern features smooth animations, intuitive navigation, detailed car listings with pricing and fuel info, and an integrated live map for easy rental location access and trip planning.",
+        github: "https://github.com/ShahzainAhmed/CarRentalApp_BLoC",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7248251341561622528/"
+    },
+    {
+        title: "Elite Shoes",
+        image: "assets/projects/project12.png",
+        description:
+            "A stylish Flutter-based shoe shopping app offering a premium selection of footwear. Elite Shoes delivers a smooth and visually engaging shopping experience with modern UI design, detailed product ratings, and easy access to sizes and styles.",
+        github: "https://github.com/ShahzainAhmed/Shoes_App",
+        demo: "https://www.linkedin.com/posts/shahzainahmed_getx-getxarchitecture-bloc-activity-7263906996968452096-Slwx?utm_source=share&utm_medium=member_desktop&rcm=ACoAADBAUAkBcSzTUtnIWIqWfBZuZcGwiqqsDiI"
+    },
+    {
+        title: "Cookies App",
+        image: "assets/projects/project13.png",
+        description:
+            "A delightful Flutter-based app for cookie lovers, offering a premium selection of treats with smooth animations and a modern UI. The Delicious Cookies App provides effortless browsing, real-time cart updates, and cookie ratings for a fun and seamless shopping experience.",
+        github: "https://github.com/ShahzainAhmed/Cookies_App",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7277317463157936130/"
+    },
+    {
+        title: "Luxury Timepieces",
+        image: "assets/projects/project14.png",
+        description:
+            "A dynamic and visually engaging food delivery app UI built with Flutter. Designed with a focus on usability and smooth navigation, the app connects all data through models to ensure real-time performance and a seamless user experience.",
+        github: "https://github.com/ShahzainAhmed/Luxury_Timepieces",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7278840846559367169/"
+    },
+    {
+        title: "Motorbike App",
+        image: "assets/projects/project15.png",
+        description:
+            "A dynamic Flutter-based app showcasing high-performance motorbikes with detailed specs, prices, and ratings. The Motorbike Showcase App features smooth animations, a modern UI, and effortless navigation for exploring top-tier bikes from renowned brands.",
+        github: "https://github.com/ShahzainAhmed/Motorbike_App",
+        demo: "https://www.linkedin.com/posts/shahzainahmed_getx-bloc-flutterbloc-activity-7281992116837240832-tePT?utm_source=share&utm_medium=member_desktop&rcm=ACoAADBAUAkBcSzTUtnIWIqWfBZuZcGwiqqsDiI"
+    },
+    {
+        title: "Travel Lens",
+        image: "assets/projects/project16.png",
+        description:
+            "A visually immersive Flutter travel app that helps users explore South America’s hidden gems and iconic landmarks. Travel Lens features stunning imagery, smooth animations, and detailed ratings and descriptions for effortless trip planning and unforgettable adventures.",
+        github: "https://github.com/ShahzainAhmed/TravelLens",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7283377349436297217/"
+    },
+    {
+        title: "Virtual Exhibition",
+        image: "assets/projects/project17.png",
+        description:
+            "A modern Flutter-based app that redefines the art experience through virtual exhibitions. Virtual Exhibition offers elegant artwork displays, smooth animations, and detailed descriptions, creating an immersive and intuitive platform for exploring art and culture.",
+        github: "https://github.com/ShahzainAhmed/VirtualExhibitionApp",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7314567307198967808/"
+    },
+    {
+        title: "Modern Movie",
+        image: "assets/projects/project18.png",
+        description:
+            "A sleek Flutter-based movie browsing app offering a premium cinematic experience. Modern Movie App features movie details like ratings, genres, duration, and posters, along with storylines, trending and top-rated categories, and a clean, modern interface with smooth animations.",
+        github: "https://github.com/ShahzainAhmed/ModernMovieApp",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7333428056990056448/"
+    },
+    {
+        title: "AI Chatbot",
+        image: "assets/projects/project19.png",
+        description:
+            "A modern Flutter-based AI Chatbot app inspired by ChatGPT, delivering real-time intelligent responses through OpenRouter's GPT-3.5-turbo. It features a sleek dark UI, smooth auto-scroll, and a fast, intuitive interface for a natural chat experience.",
+        github: "https://github.com/ShahzainAhmed/AI_ChatBot",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7341917013293867010/"
+    },
+    {
+        title: "Luxury Car Rental",
+        image: "assets/projects/project20.png",
+        description:
+            "A modern Flutter-based car rental app featuring Pakistani vehicles and core booking functionalities. It offers secure authentication, smooth payments, and real-time notifications for a seamless user experience, powered by Firebase Auth, Stripe integration, and FCM.",
+        github: "https://github.com/ShahzainAhmed/CarRental_Stripe_Firebase",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7380346662524731392/"
+    },
+    {
+        title: "Modern Perfume",
+        image: "assets/projects/project21.png",
+        description:
+            "A sleek and user-friendly Flutter perfume app that lets users explore fragrances, view detailed descriptions and prices, and save favorites. Modern Perfume App features smooth animations and an elegant interface, delivering an enjoyable and immersive browsing experience.",
+        github: "https://github.com/ShahzainAhmed/Modern_Perfume_App",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7382424006341156864/"
+    },
+    {
+        title: "Digital Book Shelf",
+        image: "assets/projects/project22.png",
+        description:
+            "A modern Flutter app that transforms book browsing into an elegant experience. Digital Book Shelf features a beautifully animated bookshelf, detailed book pages with author info and ratings, preview options, and a clean, seamless interface for book lovers.",
+        github: "https://github.com/ShahzainAhmed/Digital_Book_Shelf",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7384126992108990464/"
+    },
+    {
+        title: "Travel Dubai",
+        image: "assets/projects/project23.png",
+        description:
+            "A visually stunning Flutter app that lets users explore Dubai's iconic landmarks and attractions through interactive swipeable cards. Travel Dubai features a clean, modern UI with pricing details for a smooth and engaging exploration experience.",
+        github: "https://github.com/ShahzainAhmed/Travel_Dubai",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7385600608793300992/"
+    },
+    {
+        title: "Arts Gallery",
+        image: "assets/projects/project24.png",
+        description:
+            "A modern Flutter app designed for art enthusiasts, Arts Gallery showcases a curated collection of artworks with stunning visuals and smooth animations. It features a clean, elegant interface that offers an inspiring and immersive art-viewing experience.",
+        github: "https://github.com/ShahzainAhmed/Art_Gallery",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7386687848151683072/"
     },
 ];
