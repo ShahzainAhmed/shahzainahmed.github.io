daf3c12ccca3fbed04253b6e8b20aec78e3b59eb
Shahzain Ahmed <shahzainahmed57@gmail.com>
2025-10-28 12:47:16 +0500

    Add project data for various Flutter applications

---
+++ projects_data.js
@@ -0,0 +1,74 @@
+const projects = [
+    {
+        title: "E-Commerce Store",
+        image: "assets/projects/project1.png",
+        description:
+            "A full-featured Flutter e-commerce solution integrating real-time APIs for dynamic content and pricing. Delivers an immersive shopping experience with smooth animations and core features like product favoriting.",
+        github: "https://github.com/ShahzainAhmed/E_Commerce_Store",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7163427844902043649/"
+    },
+    {
+        title: "Car Rental",
+        image: "assets/projects/project2.png",
+        description:
+            "A sleek and responsive car rental app UI built with Flutter. Features efficient data models for displaying detailed car specifications and a visually appealing design that delivers an exceptional user experience for car enthusiasts.",
+        github: "https://github.com/ShahzainAhmed/CarRentalApp",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7092476550561902592/"
+    },
+    {
+        title: "Travel Africa",
+        image: "assets/projects/project3.png",
+        description:
+            "A beautifully designed and responsive travel app UI built with Flutter. Focused on delivering a smooth user experience through efficient data handling, clean architecture, and stunning visuals that capture the spirit of travel.",
+        github: "https://github.com/ShahzainAhmed/TravelAfrica",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7091486788803805184/"
+    },
+    {
+        title: "Perfume Maker",
+        image: "assets/projects/project4.png",
+        description:
+            "A dynamic and visually appealing mobile application built with Flutter to showcase a wide range of perfumes and their details. Features an intuitive interface with real-time data integration through models, ensuring smooth navigation and an engaging user experience.",
+        github: "https://github.com/ShahzainAhmed/PerfumeMakerApp",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7063481759492976640/?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A7063481759492976640%29"
+    },
+    {
+        title: "Food Delivery",
+        image: "assets/projects/project5.png",
+        description:
+            "A dynamic and visually engaging food delivery app UI built with Flutter. Designed with a focus on usability and smooth navigation, the app connects all data through models to ensure real-time performance and a seamless user experience.",
+        github: "https://github.com/ShahzainAhmed/FoodDeliveryApp",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7062390276979310592/?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A7062390276979310592%29"
+    },
+    {
+        title: "Blog App",
+        image: "assets/projects/project6.png",
+        description:
+            "A Flutter-based blog application featuring a clean, modern UI for an engaging reading experience. It highlights top writers with professional visuals, showcases upcoming releases, and includes a dynamic recent blogs feed that keeps users updated with the latest content in real time.",
+        github: "https://github.com/ShahzainAhmed/BlogApp",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7156735134799405056/?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A7156735134799405056%29"
+    },
+    {
+        title: "Travel Booking",
+        image: "assets/projects/project7.png",
+        description:
+            "A dynamic and visually appealing mobile application built with Flutter to showcase a wide range of perfumes and their details. Features an intuitive interface with real-time data integration through models, ensuring smooth navigation and an engaging user experience.",
+        github: "https://github.com/ShahzainAhmed/PerfumeMakerApp",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7063481759492976640/?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A7063481759492976640%29"
+    },
+    {
+        title: "Hotel Booking",
+        image: "assets/projects/project8.png",
+        description:
+            "A visually rich hotel booking app built with Flutter, offering smooth animations and intuitive navigation. Users can explore premium hotels with detailed info, stunning visuals, ratings, and pricing, powered by accurate and efficient data models.",
+        github: "https://github.com/ShahzainAhmed/HotelBookingApp",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7175444369632600064/?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A7175444369632600064%29"
+    },
+    {
+        title: "Kusmi Tea",
+        image: "assets/projects/project9.png",
+        description:
+            "A Flutter-based blog application featuring a clean, modern UI for an engaging reading experience. It highlights top writers with professional visuals, showcases upcoming releases, and includes a dynamic recent blogs feed that keeps users updated with the latest content in real time.",
+        github: "https://github.com/ShahzainAhmed/BlogApp",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:7156735134799405056/?updateEntityUrn=urn%3Ali%3Afs_feedUpdate%3A%28V2%2Curn%3Ali%3Aactivity%3A7156735134799405056%29"
+    },
+];
